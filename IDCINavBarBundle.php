da02184d0afac5dfe5d477e75dacfa01adbf6bd1
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-11-07 12:31:07 +0100

    Add Bundle class file

---
+++ IDCINavBarBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ *
+ */
+
+namespace IDCI\Bundle\IDCINavBarBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class IDCINavBarBundle extends Bundle
+{
+}
